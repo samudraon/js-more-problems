a0b2476fe653a1b77c8ca6e368cf11b82bfdd77a
samudraon <samudraon@gmailcom>
2023-02-05 03:12:33 +0600

    greater number problem solved

--- greaterThanFive.js
+++ greaterThanFive.js
@@ -1,15 +1,15 @@
 function greaterThanFive(numbers) {
+    let count = 0;
     for (let i = 0; i < numbers.length; i++) {
         const element = numbers[i];
         // console.log(element);
         if (element > 5) {
-            console.log('YES')
-        }
-        else {
-            console.log('NO')
+            count++;
         }
     }
+    return count;
 }
 
 const numbers = [-1, 2, -3, 4, 5, 6, -7, 8, -9, 10];
 const greater = greaterThanFive(numbers);
+console.log(greater);
